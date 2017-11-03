1e844ff9086dd9224f42ff14ee780287174b96d8
Kun Chen <kc75997@uga.edu>
2017-11-03 00:26:18 -0400

    saving before rm rf

--- WebRat/__init__.py
+++ WebRat/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from WebRat.rat_report.routes import rat_report_blueprint
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 app.register_blueprint(rat_report_blueprint, url_prefix='/rest/ratReport')
 
 
 @app.route('/', methods=['GET'])
 def root_handler():
-    return 'hello from WebRat'
+    return app.send_static_file('index.html')
 
 
 @app.route('/hello', methods=['GET'])
