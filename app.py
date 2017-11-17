b5c76877a50bfb475bd066e42fd1f454d4881408
Kun Chen <kc75997@uga.edu>
2017-11-17 03:23:19 -0500

    added env

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from WebRat import app
+import os
 
 
 def start_server():
-    app.run(debug=True)
+    if os.environ['env'] == 'pr':
+        app.run()
+    else:
+        app.run(debug=True)
 
 if __name__ == '__main__':
     start_server()
